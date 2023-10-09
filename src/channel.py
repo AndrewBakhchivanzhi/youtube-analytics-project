c44797f267117e43fb5630816b39e0a24653b15b
Andrey <bakhcha_ml@mail.ru>
2023-10-09 23:20:21 +1000

    add_channel_edit

--- src/channel.py
+++ src/channel.py
@@ -1,17 +1,16 @@
 import os
 import json
 
-from helper.youtube_api_manual import youtube
 from googleapiclient.discovery import build
-
 
 class Channel:
     """Класс для ютуб-канала"""
-    api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
         self.channel_id = channel_id
         self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
